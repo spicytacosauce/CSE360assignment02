c1e0bf6e76700de237d95cee17f042d838053b03
Jefe <Jserkin@asu.edu>
2020-10-07 21:10:18 -0700

    inital

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assignment02;
+
+/**
+ * @author Jeffery Erskine CSE 360 Assignment 2
+ * This program adds the values from the main to 
+ * print the to string "0 + 4 – 2 + 5"
+ * myCalculator.add (4); 
+ * myCalculator.subtract (2); 
+ * myCalculator.add(5);
+ */
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * getTotal method will return the value of total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * add method adds the value to the total
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * subtract method subtracts the value from the total
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * The toString will be called by the main
+   * to print the results. 
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * the clear method sets values back to 0
+   */
+  public void clear() {
+  }
+}
